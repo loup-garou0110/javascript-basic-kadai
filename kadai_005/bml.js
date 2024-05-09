69c409bbf290fb2486c37743adabbf47ad811f48
loup-garou0110 <166971804+loup-garou0110@users.noreply.github.com>
2024-05-09 15:20:06 +0900

    ファイルアップロード

---
+++ kadai_005/bml.js
@@ -0,0 +1,14 @@
+// 定数に体重を代入
+const weight = 68;
+// コンソールへの出力
+console.log(weight);
+
+// 定数に身長を代入
+const height =1.7;
+// コンソールへの出力
+console.log(height);
+
+// 定数にbmiを代入
+const bmi = (weight / (height * height));
+// コンソールへの出力
+console.log(bmi);
