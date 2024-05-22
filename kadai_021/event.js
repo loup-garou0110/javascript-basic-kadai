b3ed5e1c8c031091bfaf89e876651f6d6974db67
loup-garou0110 <166971804+loup-garou0110@users.noreply.github.com>
2024-05-22 21:18:32 +0900

    再アップロード

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,12 +1,10 @@
 // btnというidを持つHTML要素に定数を代入
-const button = document.getElementById("btn");
+const btn = document.getElementById("btn");
 //textというidを持つHTML要素に定数を代入
-const txt =document.getElementById("text");
+const text =document.getElementById("text");
 
-button.addEventListener("click", () => {
+btn.addEventListener("click", () => {
   setTimeout(() => {
-    const childtxt = document.createElement("h2");
-    childtxt.textContent="ボタンをクリックしました";
-    txt.appendChild(childtxt);
+    text.textContent="ボタンをクリックしました";
   },2000);
 });
